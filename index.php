e214a31e3bdd1f3943076f57f3a684a46792846d
pedro2806 <isc.pedro.mtz@gmail.com>
2026-01-06 11:35:26 -0600

    se agrega a corro para captura de planeacion de serviciosomar.corro
    se agrega a corro para captura de planeacion de serviciosomar.corro

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
     </style>
 
 <?php
-    $usuariosRegistran = array(212, 14, 42, 161, 403, 183, 521, 276, 26, 147, 189, 177, 45, 26, 525, 435, 489, 523, 298, 81, 203, 8, 278, 206, 123, 516, 5280,525);
+    $usuariosRegistran = array(212, 14, 42, 161, 403, 183, 521, 276, 26, 147, 189, 177, 45, 26, 525, 435, 489, 523, 298, 81, 203, 8, 278, 206, 123, 516, 5280,525,288);
 
     if (in_array($_COOKIE['noEmpleado'], $usuariosRegistran)) {
         // El usuario tiene permiso para ver la página

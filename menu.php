27329dc889be2df200194a9c9cc0aa64bcd68836
pedro2806 <isc.pedro.mtz@gmail.com>
2026-01-07 11:19:01 -0600

    Permisos para aelectrica
    Permisos para aelectrica

--- menu.php
+++ menu.php
@@ -37,7 +37,7 @@
 <?php
 //81 203 8  usuarios norte
 //USUARIOS QUE PUEDEN REGISTRAR ACTIVIDADES
-$usuariosRegistran = array(212, 14, 42, 161, 403, 183, 521, 276, 26, 147, 189, 177, 45, 26, 525, 435, 489, 523, 298, 81, 203, 8, 278, 206, 123, 516, 5280, 525,288);
+$usuariosRegistran = array(212, 14, 42, 161, 403, 183, 521, 276, 26, 147, 189, 177, 45, 26, 525, 435, 489, 523, 298, 81, 203, 8, 278, 206, 123, 516, 5280, 525, 288, 360, 487);
 
 if (in_array($_COOKIE['noEmpleado'], $usuariosRegistran)) {
 ?>

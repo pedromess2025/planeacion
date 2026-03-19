725309329df6d4a797ef5be144203370af16ea35
Sebastian <sgutierrez26@alumnos.uaq.mx>
2026-03-19 15:02:43 -0600

    Solucion modal
    Se agrego la librería bs5 de forma local para poder ab5rir y cerrar modales sin bug

--- entradaDetalleEntradas.php
+++ entradaDetalleEntradas.php
@@ -314,7 +314,8 @@
     <!-- Bootstrap core JavaScript
     <script src = "vendor/jquery/jquery.min.js"></script>-->
     <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle5.min.js"></script>
 
     <!-- Core plugin JavaScript-->
     <script src="vendor/jquery-easing/jquery.easing.min.js"></script>

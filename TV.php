052bbbfb91a2ffda1431dcd91e50ec3725d613ad
pedro2806 <isc.pedro.mtz@gmail.com>
2026-04-01 11:55:27 -0600

    Update TV.php

--- TV.php
+++ TV.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Rotador con Control Manual</title>
+    <title>Planeación Semanal TV</title>
     <style>
         body, html {
             margin: 0;
